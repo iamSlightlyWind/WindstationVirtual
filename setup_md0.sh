a6110cbe9b69307185bf5709f991bbbe7835f383
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2022-03-21 20:01:35 +0700

    Update scripts

---
+++ setup_md0.sh
@@ -0,0 +1,12 @@
+./stop_md0.sh
+
+rm efi*
+
+dd if=/dev/zero of=efi1 bs=1M count=100
+dd if=/dev/zero of=efi2 bs=1M count=1
+
+chmod 666 efi*
+
+./start_md0.sh
+
+sudo parted /dev/md0 < parted.script
