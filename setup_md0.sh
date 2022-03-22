9eed72488b31f7539c5e39d162b18d9fce1f61b7
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2022-03-22 11:12:31 +0700

    Finalize setup

--- setup_md0.sh
+++ setup_md0.sh
@@ -1,10 +1,20 @@
-rm efi*
+sudo losetup -f efi1
+sudo losetup -f efi2
 
+echo  "######Pre-setup"
+rm efi*
+./stop_md0.sh
+
+echo "\n######Setting up efi files"
 dd if=/dev/zero of=efi1 bs=1M count=100
 dd if=/dev/zero of=efi2 bs=1M count=1
-
 chmod 666 efi*
 
+echo "\n######Starting md0"
 ./start_md0.sh
 
+echo "\n######Partitioning md0"
 sudo parted /dev/md0 < parted.script
+
+echo "######Final touch"
+sudo mkfs.msdos -F 32 -n EFI /dev/md0p1
